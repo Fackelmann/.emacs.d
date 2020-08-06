5c0ac10519e4b0af01af903727683a8ae877e3cf
Daniel Gonzalez Plaza <dan@danielgplaza.com>
2020-08-05 21:46:02 -0700

    Added slack

--- init.el
+++ init.el
@@ -182,15 +182,17 @@
 ;; Capture templates for: TODO tasks, Notes, appointments, phone calls, meetings, and org-protocol
 (setq org-capture-templates
       (quote (("t" "todo" entry (file "~/org/inbox.org")
-               "* TODO %?\n%U\n%A\n" :clock-in t :clock-resume t)
+               "* TODO %?\n%U\n" :clock-in t :clock-resume t)
               ("n" "note" entry (file "~/org/inbox.org")
-               "* %? :NOTE:\n%U\n%A\n" :clock-in t :clock-resume t)
+               "* %? :NOTE:\n%U\n\n" :clock-in t :clock-resume t)
               ("m" "Meeting" entry (file "~/org/inbox.org")
                "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
               ("p" "Phone call" entry (file "~/org/inbox.org")
                "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
+	      ("s" "Slack" entry (file "~/org/inbox.org")
+               "* SLACK %? :SLACK:\n%U" :clock-in t :clock-resume t)
               ("h" "Habit" entry (file "~/org/inbox.org")
-               "* NEXT %?\n%U\n%A\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
+               "* NEXT %?\n%U\n\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
 
 ;; --- Magit
 ;; Bind magit C-x g
