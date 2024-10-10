8ac9e5f8025821c8952f9c23042d1f293d49f9ee
Dani <dan@danielgplaza.com>
2024-10-09 21:36:41 -0700

    Moving org loading even earlier.
    That should do it. I had to move straight.el bootstraping to
    init.el. The problem seems to happen when org is loaded before
    org-babel-load-file

--- init.el
+++ init.el
@@ -1,6 +1,31 @@
 (defconst emacs-start-time (current-time))
 
-(require 'org)
+;; I'm using straight.el for reproducibility and source control:
+(setq straight-base-dir "~/.emacs.d/straight/")
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+	"straight/repos/straight.el/bootstrap.el"
+	(or (bound-and-true-p straight-base-dir)
+	    user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+;; Ensure use-package uses straight.el by default
+(straight-use-package 'use-package)
+
+;;And make it the default with use-package
+(setq straight-use-package-by-default t)
+
+;; Load org package right after that and before org-babel-load file to avoid issues with
+;; org versions
+(straight-use-package 'org)
 
 (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
 
